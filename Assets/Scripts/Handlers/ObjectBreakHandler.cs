33025f08800132d5463f7481dd5ae780ff995192
Justin Kaiser <100805805+JKK890@users.noreply.github.com>
2024-04-01 16:42:49 -0300

    added features to level1

--- Assets/Scripts/Handlers/ObjectBreakHandler.cs
+++ Assets/Scripts/Handlers/ObjectBreakHandler.cs
@@ -5,12 +5,17 @@ public class CrateHitHandler : HitHandler {
     public GameObject drop;
     public Vector2 dropOffset;
 
+    public delegate void BreakAction();
+    public event BreakAction OnBreak;
+
     public override void HandleHit() {
         var go = gameObject;
         var pos = go.transform.position;
+        OnBreak?.Invoke();
         Destroy(go);
         var result = Instantiate(drop);
         result.transform.position = pos + (Vector3)dropOffset;
     }
+    
 }
 }
---
+++ Assets/Scripts/Objectives/ObjectBreakObjective.cs
@@ -0,0 +1,5 @@
+﻿namespace Objectives {
+    public class ObjectBreakObjective {
+        
+    }
+}
